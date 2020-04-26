b4dca0674a4dd14454ca8e0e5ae53f3b858916bb
faridho <faridho45@gmail.com>
2020-04-26 16:22:26 +0700

    first commit

---
+++ DarkMode/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  DarkMode
+//
+//  Created by Faridho on 25/04/20.
+//  Copyright © 2020 Faridho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var changeButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //overrideUserInterfaceStyle = .light
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.white
+        } else {
+            changeButton.tintColor = UIColor.systemBlue
+        }
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.white
+        } else {
+            changeButton.tintColor = UIColor.systemBlue
+        }
+    }
+
+
+}
+
